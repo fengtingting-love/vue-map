01fda32db70a64fdc10982ee725e5aac46e7d4c6
冯婷婷 <fengtingting6210@gmail.com>
2020-07-06 10:41:28 +0800

    feature:新增高德API城市码调用

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    123
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {}
+  },
+  methods:{
+    //获取高德信息
+    getPosition(){
+      // 实例化城市查询类
+      const citySearch = new window.AMap.CitySearch();
+          //自动获取用户IP，返回当前城市
+          citySearch.getLocalCity(function (status, result) {
+            //如果请求成功无误
+            if (status === 'complete' && result.info === 'OK') {
+              // resolve(result.adcode)
+              console.log(result)
+            } 
+            //如果请求有误
+            else {
+              // reject(result)
+            }
+          });
+    }
+  },
+  mounted(){
+    this.getPosition();
   }
 }
 </script>
