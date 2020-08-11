cb28d2908df0c05af7a62310dc0832c4ba318bf7
冯婷婷 <fengtingting6210@gmail.com>
2020-08-11 23:00:20 +0800

    refactor:render函数

--- src/components/FlexiableSlot.js
+++ src/components/FlexiableSlot.js
@@ -1,14 +1,18 @@
 export default {
-    render: function (createElement) {
-      return createElement(
-        'h' + this.level,   // 标签名称
-        this.$slots.default // 子节点数组
-      )
+  render: function (createElement) {
+    return createElement(
+      'h' + this.level,   // 标签名称
+      this.$slots.default,// 子节点数组
+    )
+  },
+  props: {
+    level: {
+      type: Number,
+      required: true,
     },
-    props: {
-      level: {
-        type: Number,
-        required: true
-      }
+    content: {
+      type: String,
+      required: true,
     }
+  }
 }
--- src/views/SelfComponent.vue
+++ src/views/SelfComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="selfComponent">
     <br/>
-    <FlexiableSlot :level="1">
+    <FlexiableSlot :level="1" :content="title1">
       【一级标题】level值为1，表示渲染的是h1标签
     </FlexiableSlot>
-    <FlexiableSlot :level="2">
+    <FlexiableSlot :level="2" :content="title2">
       【二级标题】level值为2，表示渲染的是h2标签
     </FlexiableSlot>
   </div>
@@ -15,6 +15,12 @@
   export default {
     components: {
       FlexiableSlot,
+    },
+    data(){
+      return {
+        title1:'h1传入的内容',
+        title2:'h2传入的内容',
+      }
     }
   }
 </script>
